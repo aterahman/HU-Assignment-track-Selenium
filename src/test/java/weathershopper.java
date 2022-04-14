a76293dda0fec91839e5cf6334b567d39180a248
Rahman <aterahman@deloitte.com>
2022-04-14 13:07:57 +0530

    Added method to get the temperature displayed on screen

--- src/test/java/weathershopper.java
+++ src/test/java/weathershopper.java
@@ -1,7 +1,11 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
@@ -30,5 +34,50 @@ public class weathershopper
         driver.manage().timeouts().implicitlyWait(3000, TimeUnit.MILLISECONDS);
     }
 
+    //recurring method ot find the desired element by providing xpath
+    public WebElement getelement(String xpath)
+    {
+        //find the element with the xpath provided
+        WebElement element = driver.findElement(By.xpath(xpath));
+
+        //returns element back to the method which called this method
+        return element;
+    }
+
+    //method to get the temperature
+    @Test(priority = 1)
+    public void gettemperature()
+    {
+        //gets the element that displays the temperature on the screen
+        WebElement temperature = getelement("/html/body/div/div[2]/div/span");
+
+        //gets the text of the element storing the temperature
+        String temp = temperature.getText();
+
+        //getting only the digits of the temperature stored in the string
+        String temperaturedigits = "";
+        for(int i=0; i<temp.length(); i++)
+        {
+            char c = temp.charAt(i);
+
+            //checks whether the character is a digit
+            if(Character.isDigit(c))
+                temperaturedigits = temperaturedigits + c;
+            else
+                break;
+        }
+
+        int digitaltemperature = Integer.parseInt(temperaturedigits);
+        System.out.println(digitaltemperature);
+    }
+
     
+
+    //method to close the driver
+    @AfterClass
+    public void close()
+    {
+        driver.close();
+        driver.quit();
+    }
 }
