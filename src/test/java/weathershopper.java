00fd70bf58f43d8dfc2d8d92dd14a06828f26ee7
Rahman <aterahman@deloitte.com>
2022-04-14 12:50:20 +0530

    added method to open the desired url

--- src/test/java/weathershopper.java
+++ src/test/java/weathershopper.java
@@ -3,10 +3,13 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeSuite;
 
+import java.util.concurrent.TimeUnit;
+
 public class weathershopper
 {
     WebDriver driver;
 
+    //method to initilise WebDriver and the chrome driver
     @BeforeSuite
     public void setDriver()
     {
@@ -14,9 +17,18 @@ public class weathershopper
       driver = new ChromeDriver();
     }
 
+    //method to open the url
     @BeforeClass
-    public void openurl
+    public void openurl()
     {
+        //opening the url
         driver.get("https://weathershopper.pythonanywhere.com/");
+
+        //maximizing the window
+        driver.manage().window().maximize();
+
+        driver.manage().timeouts().implicitlyWait(3000, TimeUnit.MILLISECONDS);
     }
+
+    
 }
