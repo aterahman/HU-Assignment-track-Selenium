d1525e835292fea95434551fa86f2c5eef51b59d
Rahman <aterahman@deloitte.com>
2022-04-16 22:28:51 +0530

    added the methods abck to the extra class, have added extra methods to store different prices and product names and to compare prices

--- src/test/java/productlist.java
+++ src/test/java/productlist.java
@@ -3,8 +3,7 @@ import org.testng.annotations.Test;
 
 import java.util.ArrayList;
 
-public class productlist extends weathershopper
-{
+public class productlist extends weathershopper {
 
     //first row names
     ///html/body/div[1]/div[2]/div[1]/p[1]
@@ -39,50 +38,115 @@ public class productlist extends weathershopper
     //storing all the details in a workbook
 
 
+    ArrayList<String> productnames = new ArrayList<>();
+    ArrayList<String> productprices = new ArrayList<>();
+    ArrayList<WebElement> buttons = new ArrayList<>();
+    ArrayList<String> aloe = new ArrayList<>();
+    ArrayList<String> almond = new ArrayList<>();
+    ArrayList<String> spf30 = new ArrayList<>();
+    ArrayList<String> spf50 = new ArrayList<>();
+    ArrayList<String> aloeprice = new ArrayList<>();
+    ArrayList<String> almondprice = new ArrayList<>();
+    ArrayList<String> spf30price = new ArrayList<>();
+    ArrayList<String> spf50price = new ArrayList<>();
+    ArrayList<WebElement> aloebuttons = new ArrayList<>();
+    ArrayList<WebElement> almondbuttons = new ArrayList<>();
+    ArrayList<WebElement> spf30buttons = new ArrayList<>();
+    ArrayList<WebElement> spf50buttons = new ArrayList<>();
 
-        @Test(priority = 5)
-        public void storeproducts() {
-            ArrayList<String> productnames = new ArrayList<>();
-
-            int num = 0;
-
-            for (int i = 2; i <= 3; i++) {
-                for (int j = 1; j <= 3; j++) {
-                    String xlpath = "/html/body/div[1]/div[" + Integer.toString(i) + "]/div[" + Integer.toString(j) + "]/p[1]";
-                    WebElement productname = getelement(xlpath);
-                    productnames.add(productname.getText());
 
 
-                }
+    public void storeproducts() {
+
+        int num = 0;
+
+        for (int i = 2; i <= 3; i++) {
+            for (int j = 1; j <= 3; j++) {
+                String xlpath = "/html/body/div[1]/div[" + Integer.toString(i) + "]/div[" + Integer.toString(j) + "]/p[1]";
+                WebElement productname = getelement(xlpath);
+                productnames.add(productname.getText());
             }
-            System.out.println(productnames);
         }
+        System.out.println(productnames);
+    }
 
-        public void storeprices()
-        {
-            ArrayList<String> productprices = new ArrayList<>();
 
-            for(int i=2;i<=3;i++)
-            {
-                for(int j = 1 ; j<=3;j++)
-                {
-                    String xlpath = "/html/body/div[1]/div["+Integer.toString(i)+"]/div["+ Integer.toString(j)+"]/p[2]";
-                    WebElement productprice = getelement(xlpath);
-                    String p  = productprice.getText();
-                    String price  = "";
-                    for(int k = 0; k<p.length();k++)
-                    {
-                        char ch = p.charAt(k);
-                        if(Character.isDigit(ch))
-                            price=price+ch;
-                    }
-                    productprices.add(price);
-
+    public void storeprices() {
+        for (int i = 2; i <= 3; i++) {
+            for (int j = 1; j <= 3; j++) {
+                String xlpath = "/html/body/div[1]/div[" + Integer.toString(i) + "]/div[" + Integer.toString(j) + "]/p[2]";
+                WebElement productprice = getelement(xlpath);
+                String p = productprice.getText();
+                String price = "";
+                for (int k = 0; k < p.length(); k++) {
+                    char ch = p.charAt(k);
+                    if (Character.isDigit(ch))
+                        price = price + ch;
                 }
+                productprices.add(price);
+
+            }
+        }
+        System.out.println(productprices);
+    }
+
+    public void sortproducts() {
+        for (int i = 0; i < productnames.size(); i++) {
+            String product = productnames.get(i);
+            String price = productprices.get(i);
+            if (product.contains("Aloe")) {
+                aloe.add(product);
+                aloeprice.add(product);
+            }
+            else if (product.contains("Almond")) {
+                almond.add(product);
+                almondprice.add(product);
+            }
+            else if (product.contains("SPF-30")) {
+                spf30.add(product);
+                spf30price.add(product);
+            }
+           else if (product.contains("SPF-50")) {
+                spf50.add(product);
+                spf50price.add(product);
             }
 
         }
-
-
 
     }
+
+    public void comparemoisturizers()
+    {
+        int leastaloeprice = 0;
+        for(int i =0;i<aloeprice.size();i++)
+        {
+            int price = Integer.parseInt(aloeprice.get(i));
+
+            for(int j=i+1;j<aloeprice.size();j++)
+            {
+                int price1 = Integer.parseInt(aloeprice.get(j));
+                if(price>price1)
+                {
+                    int temp = price1;
+                    price1= leastaloeprice;
+                    leastaloeprice=price1;
+                }
+            }
+        }
+        int leastalmondprice = 0;
+        for(int i =0;i<almondprice.size();i++)
+        {
+            int price = Integer.parseInt(aloeprice.get(i));
+
+            for(int j=i+1;j<aloeprice.size();j++)
+            {
+                int price1 = Integer.parseInt(aloeprice.get(j));
+                if(price>price1)
+                {
+                    leastalmondprice=j;
+                }
+            }
+        }
+    }
+}
+
