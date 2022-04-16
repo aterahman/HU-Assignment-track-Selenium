15a640c2214e4e5e960b06fb94e5fd9ef6dab543
Rahman <aterahman@deloitte.com>
2022-04-16 17:39:04 +0530

    Added testng listener files

--- src/test/java/testnglistener.java
+++ src/test/java/testnglistener.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class testnglistener {
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class testnglistener extends weathershopper implements ITestListener
+{
+    @Override
+    public void onTestStart(ITestResult result)
+    {
+
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult result)
+    {
+        System.out.println("PASSED TEST");
+        passed();
+    }
+
+    @Override
+    public void onTestFailure(ITestResult result)
+    {
+        System.out.println("FAILED TEST");
+        failed();
+    }
+
+    @Override
+    public void onTestSkipped(org.testng.ITestResult result)
+    {
+        /* compiled code */
+    }
 }
