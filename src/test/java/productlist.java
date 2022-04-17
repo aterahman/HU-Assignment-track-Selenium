4ab98451a999e7d11b7429eceb256f76b9f4565f
Rahman <aterahman@deloitte.com>
2022-04-17 05:44:26 +0530

    this is the extended class that stores, sorts and adds products to the cart as per the prices

--- src/test/java/productlist.java
+++ src/test/java/productlist.java
@@ -1,4 +1,5 @@
 import org.openqa.selenium.WebElement;
+import org.testng.SkipException;
 import org.testng.annotations.Test;
 
 import java.util.ArrayList;
@@ -38,27 +39,25 @@ public class productlist extends weathershopper {
     //storing all the details in a workbook
 
 
-    ArrayList<String> productnames = new ArrayList<>();
-    ArrayList<String> productprices = new ArrayList<>();
-    ArrayList<WebElement> buttons = new ArrayList<>();
-    ArrayList<String> aloe = new ArrayList<>();
-    ArrayList<String> almond = new ArrayList<>();
-    ArrayList<String> spf30 = new ArrayList<>();
-    ArrayList<String> spf50 = new ArrayList<>();
-    ArrayList<String> aloeprice = new ArrayList<>();
-    ArrayList<String> almondprice = new ArrayList<>();
-    ArrayList<String> spf30price = new ArrayList<>();
-    ArrayList<String> spf50price = new ArrayList<>();
-    ArrayList<WebElement> aloebuttons = new ArrayList<>();
-    ArrayList<WebElement> almondbuttons = new ArrayList<>();
-    ArrayList<WebElement> spf30buttons = new ArrayList<>();
-    ArrayList<WebElement> spf50buttons = new ArrayList<>();
+   public ArrayList<String> productnames = new ArrayList<>();
+   public ArrayList<String> productprices = new ArrayList<>();
+   public ArrayList<WebElement> buttons = new ArrayList<>();
+   public ArrayList<String> aloe = new ArrayList<>();
+   public ArrayList<String> almond = new ArrayList<>();
+   public ArrayList<String> spf30 = new ArrayList<>();
+   public ArrayList<String> spf50 = new ArrayList<>();
+   public ArrayList<String> aloeprice = new ArrayList<>();
+   public ArrayList<String> almondprice = new ArrayList<>();
+   public ArrayList<String> spf30price = new ArrayList<>();
+   public ArrayList<String> spf50price = new ArrayList<>();
+   public ArrayList<WebElement> aloebuttons = new ArrayList<>();
+   public ArrayList<WebElement> almondbuttons = new ArrayList<>();
+   public ArrayList<WebElement> spf30buttons = new ArrayList<>();
+   public ArrayList<WebElement> spf50buttons = new ArrayList<>();
 
 
-
+    @Test(priority = 5)
     public void storeproducts() {
-
-        int num = 0;
 
         for (int i = 2; i <= 3; i++) {
             for (int j = 1; j <= 3; j++) {
@@ -70,7 +69,7 @@ public class productlist extends weathershopper {
         System.out.println(productnames);
     }
 
-
+    @Test(priority = 6)
     public void storeprices() {
         for (int i = 2; i <= 3; i++) {
             for (int j = 1; j <= 3; j++) {
@@ -90,63 +89,113 @@ public class productlist extends weathershopper {
         System.out.println(productprices);
     }
 
+    @Test(priority = 7)
+    public void storebuttons() {
+
+        for (int i = 2; i <= 3; i++) {
+            for (int j = 1; j <= 3; j++) {
+                String xlpath = "/html/body/div[1]/div[" + Integer.toString(i) + "]/div[" + Integer.toString(j) + "]/button";
+                WebElement productbutton = getelement(xlpath);
+                buttons.add(productbutton);
+            }
+        }
+        System.out.println(productnames);
+    }
+
+    @Test(priority = 8)
     public void sortproducts() {
         for (int i = 0; i < productnames.size(); i++) {
             String product = productnames.get(i);
             String price = productprices.get(i);
+            WebElement button = buttons.get(i);
             if (product.contains("Aloe")) {
                 aloe.add(product);
-                aloeprice.add(product);
+                aloeprice.add(price);
+                aloebuttons.add(button);
             }
             else if (product.contains("Almond")) {
                 almond.add(product);
-                almondprice.add(product);
+                almondprice.add(price);
+                almondbuttons.add(button);
             }
             else if (product.contains("SPF-30")) {
                 spf30.add(product);
-                spf30price.add(product);
+                spf30price.add(price);
+                spf30buttons.add(button);
             }
            else if (product.contains("SPF-50")) {
                 spf50.add(product);
-                spf50price.add(product);
+                spf50price.add(price);
+                spf50buttons.add(button);
             }
 
         }
 
     }
 
-    public void comparemoisturizers()
+    @Test(priority = 9)
+    public void comparemoisturizers() {
+        if (moisturizer) {
+            int leastaloeprice = 0;
+            for (int i = 1; i < aloeprice.size(); i++) {
+                int price = Integer.parseInt(aloeprice.get(leastaloeprice));
+                int price1 = Integer.parseInt(aloeprice.get(i));
+                if (price > price1) {
+                    leastaloeprice = i;
+                }
+            }
+
+            int leastalmondprice = 0;
+            for (int i = 1; i < almondprice.size(); i++) {
+
+                int price = Integer.parseInt(almondprice.get(leastalmondprice));
+                int price1 = Integer.parseInt(almondprice.get(i));
+                if (price > price1) {
+                    leastalmondprice = i;
+                }
+
+            }
+            aloebuttons.get(leastaloeprice).click();
+            almondbuttons.get(leastalmondprice).click();
+
+        } else {
+            throw new SkipException("");
+        }
+    }
+
+    @Test(priority = 10)
+         public void comparesunscreens() {
+        if (sunscreen) {
+            int leastspf30price = 0;
+            for (int i = 1; i < spf30price.size(); i++) {
+                int price = Integer.parseInt(spf30price.get(leastspf30price));
+                int price1 = Integer.parseInt(spf30price.get(i));
+                if (price > price1) {
+                    leastspf30price = i;
+                }
+            }
+
+            int leastspf50price = 0;
+            for (int i = 1; i < spf50price.size(); i++) {
+
+                int price = Integer.parseInt(spf50price.get(leastspf50price));
+                int price1 = Integer.parseInt(spf50price.get(i));
+                if (price > price1) {
+                    leastspf50price = i;
+                }
+
+            }
+            spf30buttons.get(leastspf50price).click();
+            spf50buttons.get(leastspf50price).click();
+
+        }
+
+    else
     {
-        int leastaloeprice = 0;
-        for(int i =0;i<aloeprice.size();i++)
-        {
-            int price = Integer.parseInt(aloeprice.get(i));
-
-            for(int j=i+1;j<aloeprice.size();j++)
-            {
-                int price1 = Integer.parseInt(aloeprice.get(j));
-                if(price>price1)
-                {
-                    int temp = price1;
-                    price1= leastaloeprice;
-                    leastaloeprice=price1;
-                }
-            }
-        }
-        int leastalmondprice = 0;
-        for(int i =0;i<almondprice.size();i++)
-        {
-            int price = Integer.parseInt(aloeprice.get(i));
-
-            for(int j=i+1;j<aloeprice.size();j++)
-            {
-                int price1 = Integer.parseInt(aloeprice.get(j));
-                if(price>price1)
-                {
-                    leastalmondprice=j;
-                }
-            }
-        }
+        throw new SkipException("");
     }
-}
+
+    }
+    }
+
 
