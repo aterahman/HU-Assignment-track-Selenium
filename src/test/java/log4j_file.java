a92e15dcc71aac15b9f595f302f8cf66590c262a
Rahman <aterahman@deloitte.com>
2022-04-17 07:25:20 +0530

    log4j protperties file of the program

--- src/test/java/log4j_file.java
+++
@@ -1,7 +0,0 @@
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-public class log4j_file {
-    Logger logger = LogManager.getLogger(log4j_file.class.getName());
-
-}
