3cbbdc2221e8dc65f65da6a0fe0c8d2cbaf4059d
Rahman <aterahman@deloitte.com>
2022-04-17 07:26:26 +0530

    updated the tesng listener class file of the program

--- src/test/java/testnglistener.java
+++ src/test/java/testnglistener.java
@@ -1,26 +1,31 @@
+import org.apache.log4j.Logger;
+import org.apache.log4j.LogManager;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
 public class testnglistener extends weathershopper implements ITestListener
 {
+
+
     @Override
     public void onTestStart(ITestResult result)
     {
+        log.info("Test has started"+result);
 
     }
 
     @Override
     public void onTestSuccess(ITestResult result)
     {
-        System.out.println("PASSED TEST");
-        passed();
+        log.info("PASSED TEST "+result);
+
     }
 
     @Override
     public void onTestFailure(ITestResult result)
     {
-        System.out.println("FAILED TEST");
-        failed();
+        log.info("FAILED TEST "+result);
+
     }
 
     @Override
